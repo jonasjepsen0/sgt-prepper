b1d2242cf2e24e913ef76af0ee90ee78f6343e1b
jonasjepsen0 <jonasjepsen0@gmail.com>
2026-04-22 09:58:44 +0200

    Custom events og event-parametre

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ import renderFooter from "./views/partials/footerView.js"
 import navController from "./controllers/navController.js"
 import { initRouter } from "./router/router.js"
 
+let startTid = Date.now()
+
+window.addEventListener("beforeunload", () => {
+    const sekunder = Math.round((Date.now() - startTid) / 1000)
+    gtag("event", "tid_paa_side", {
+        sekunder: sekunder
+    })
+})
+
 const initApp = () => {
   renderHeader()
   navController()
